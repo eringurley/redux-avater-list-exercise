d93d909619f92732b2d77084c4ca7fe47dc81e64
Erin Gurley <erinmgurley@gmail.com>
2019-08-30 16:29:42 -0700

    updated for routing and details page

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import AllCharacters from '../containers/AllCharacters';
 import { Provider } from 'react-redux';
 import store from '../Store';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import CharacterById from '../containers/CharacterById';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <AllCharacters />
-    </Provider> 
+    <Router>
+      <Switch>
+        <Provider store={store}>
+          <Route exact path="/" component={AllCharacters} />
+          <Route path="/characters/:_id" component={CharacterById} />
+        </Provider> 
+      </Switch>
+    </Router>
   );
 }
