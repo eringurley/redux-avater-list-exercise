6ddcae58ecb4c7f3cd2bbc5ef35d057518d2f2a8
Erin Gurley <erinmgurley@gmail.com>
2019-08-30 16:28:36 -0700

    added getcharacter reducer for details

--- src/selectors/character-selectors.js
+++ src/selectors/character-selectors.js
@@ -1,3 +1,6 @@
 export const getCharacters = state => state.characters.characters;
 export const getCharactersLoading = state => state.characters.loading;
 export const getCharactersError = state => state.characters.error;
+export const getCharacter = (state, _id) => {
+  return getCharacters(state)[state.characters.characters.findIndex(character => character._id === _id)];
+};
