4b0599622e6ffe790cc00d3fe45344be441053a8
Erin Gurley <erinmgurley@gmail.com>
2019-08-30 13:36:38 -0700

    created getCharacters function in Services api

---
+++ src/services/lastAirbenderApi.js
@@ -0,0 +1,13 @@
+export const getCharacters = () => {
+  return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Sorry, could not get characters'; 
+      return json;
+    })
+    .then(json => json.map(character => ({
+      photoUrl: character.photoUrl,
+      name: character.name,
+      _id: character._id
+    })));
+};
